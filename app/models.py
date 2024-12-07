d0ebd49673a78b36e274c9f3ed572af6c0400b6a
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2024-12-07 16:17:14 +0300

    create models

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    tasks = relationship("Task", back_populates="owner")
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    status = Column(Enum("новая", "в процессе", "завершена", name="status_enum"), default="новая")
+    priority = Column(Enum("низкий", "средний", "высокий", name="priority_enum"), default="средний")
+    deadline = Column(DateTime, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="tasks")
