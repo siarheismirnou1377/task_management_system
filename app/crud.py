bea69b5c36bc9475d19658498bc11bb39fddc9ec
siarheismirnou1377 <smirnovsergei1377@gmail.com>
2024-12-08 15:04:14 +0300

    create crud.py

---
+++ app/crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def create_user(db: Session, user: schemas.UserCreate, hashed_password):
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_tasks(db: Session, user_id: int, skip: int = 0, limit: int = 100):
+    return db.query(models.Task).filter(models.Task.owner_id == user_id).offset(skip).limit(limit).all()
+
+def create_task(db: Session, task: schemas.TaskCreate, user_id: int):
+    db_task = models.Task(**task.dict(), owner_id=user_id)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def delete_task(db: Session, task_id: int):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if task:
+        db.delete(task)
+        db.commit()
